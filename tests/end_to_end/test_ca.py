ae1ba1a8ebd7e2889942d1ea039308d9f31b8855
Adam Dangoor <adamdangoor@gmail.com>
2017-04-20 10:52:26 +0100

    Progress towards bootstrapped spike

--- tests/end_to_end/test_ca.py
+++ tests/end_to_end/test_ca.py
@@ -1,4 +1,29 @@
-def test_foo(self):
-    foo = 1
-    config = {}
-    cluster(config=config)
+class Node:
+
+    def __init__(self):
+        self.address = 1
+
+    def run_as_root(self):
+        pass
+
+
+class Cluster:
+
+    def __init__(self):
+        self.master = 1
+        self.public_agents = set([])
+        self.other_agents = set([])
+
+
+def cluster(config):
+    """
+    Deploy a cluster with a given config
+    """
+    pass
+
+
+class TestExample:
+
+    def test_foo(self):
+        config = {}
+        cluster(config=config)
